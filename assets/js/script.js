e334d11637f2e701f1b4846af223b93f03ad9e86
Koby <kobysac@gmail.com>
2023-08-29 22:48:28 -0700

    added two more sections to index.html

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const mainScreen = document.querySelector(".main");
+const quizScreen = document.querySelector(".quiz");
+const finishedScreen = document.querySelector(".finished");
+const startButton = document.querySelector("#start-button");
+
+startButton.addEventListener("click", function() {
+    // console.log("test");
+    
+})
