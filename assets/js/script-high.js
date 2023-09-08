5e51b1039094df9893d1fb39d8c0facd26714e51
Koby <kobysac@gmail.com>
2023-09-07 19:32:52 -0700

    added comments

--- assets/js/script-high.js
+++ assets/js/script-high.js
@@ -1,22 +1,31 @@
-const scoresList = document.querySelector("#scores");
+// global variables to capture important elements
+const scoresListEl = document.querySelector("#scores");
 const clearEl = document.querySelector(".clear");
 
+// load all the scores stored in local storage to the screen.
+// if clear flag is true, remove all scores from local storage.
 function loadScores(clearFlag) {
+    // if scores list is empty or the clear flag is true,
+    // set the score list element to empty
     if(localStorage.getItem("score-list") === null || clearFlag) {
-        scoresList.innerHTML = '';
-        localStorage.setItem("score-list", JSON.stringify([]));
+        scoresListEl.innerHTML = ''; // set the score list element to empty
+        localStorage.setItem("score-list", JSON.stringify([])); // set local storage to empty
         return;
     }
-    const list = JSON.parse(localStorage.getItem("score-list"));
-    for(let i=0; i<list.length; i++) {
-        const listEl = document.createElement('li');
-        listEl.textContent = list[i];
-        scoresList.append(listEl);
+    // set score list to the list of scores from local storage
+    const scoreList = JSON.parse(localStorage.getItem("score-list"));
+    for(let i=0; i<scoreList.length; i++) { // loop through the score list 
+        const listEl = document.createElement('li'); // create a new list element
+        // set the text of the new list element to the corresponding
+        // score in the score list
+        listEl.textContent = scoreList[i];
+        scoresListEl.append(listEl); // append new list element to score list element
     }
 }
 
+// clear all scores on click
 clearEl.addEventListener("click", function() {
-    loadScores(true);
+    loadScores(true); // call loadScores() with the clear flag set
 })
 
 loadScores();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// create global variables to capture important elements
 const mainScreen = document.querySelector(".main");
 const quizScreen = document.querySelector(".quiz");
 const finishedScreen = document.querySelector(".finished");
@@ -5,13 +6,19 @@ const centerFinishedEl = document.querySelector("#center-finished");
 const startButton = document.querySelector("#start-button");
 const options = document.querySelector("#answers");
 const timer = document.querySelector("#time");
-let timerInterval;
-let resultInterval;
 const scoreEl = document.querySelector("#score");
 const submitEl = document.querySelector("#submit");
 const popupEl = document.querySelector(".popup");
 const resultEl = document.querySelector("#result");
 
+// set the interval variables to be global
+// so it can be accessed by all functions
+let timerInterval;
+let resultInterval;
+
+let questionNum = 0; // a global variable that keeps track of the current question
+
+// create a global array of quiz questions
 const questions =  [
     "Javascript is an _______ language?",
     "Which of the following keywords is used to define a variable in Javascript?",
@@ -19,8 +26,8 @@ const questions =  [
     "Upon encountering empty statements, what does the Javascript Interpreter do?",
     "Which of the following methods can be used to display data in some form using Javascript?"
 ];
-let questionNum = 0;
-let score = 0;
+
+// global 2d array for quiz answers per question
 const answers = [
 ["Object-Based", "Procedural", "Object-Oriented", "None of the Above"],
 ["var", "Both var and let", "let", "None of the Above"],
@@ -29,6 +36,7 @@ const answers = [
 ["document.write()", "console.log()", "window.alert()", "All of the Above"]
 ]
 
+// global object of the correct answers for each question
 const answerKey = {
     "Javascript is an _______ language?": "Object-Oriented",
     "Which of the following keywords is used to define a variable in Javascript?": "Both var and let",
@@ -37,105 +45,108 @@ const answerKey = {
     "Which of the following methods can be used to display data in some form using Javascript?": "All of the Above"
 }
 
+// load the finish screen
 function loadFinished() {
-    // clearInterval(resultInterval);
-    quizScreen.style.display = 'none';
-    // finishedScreen.appendChild(popupEl);
-    centerFinishedEl.appendChild(popupEl);
-    finishedScreen.style.display = "flex";
-    clearInterval(timerInterval);
-
-    const score = timer.textContent;
-    scoreEl.textContent = score;
-
+    quizScreen.style.display = 'none'; // hide the quiz screen
+    centerFinishedEl.appendChild(popupEl); // show the result as quiz screen is hidden
+    finishedScreen.style.display = "flex"; // show the finished screen
+    clearInterval(timerInterval); // stop the timer count down
+    scoreEl.textContent = timer.textContent; // set the timer to the score element text
 }
 
+// loads the next question
 function loadQuestion() {
+    // if this is the last question, load the finish screen
     if(questionNum===questions.length) {
         loadFinished();
         return;
     };
+
+    // variables to hold the question and button elements
     const firstButton = document.querySelector("#first-button");
     const secondButton = document.querySelector("#second-button");
     const thirdButton = document.querySelector("#third-button");
     const fourthButton = document.querySelector("#fourth-button");
     const question = document.querySelector("#question");
 
+    // set the question and buttons text to the corresponding question and answers
     question.textContent = questions[questionNum];
     firstButton.textContent = answers[questionNum][0];
     secondButton.textContent = answers[questionNum][1];
     thirdButton.textContent = answers[questionNum][2];
     fourthButton.textContent = answers[questionNum][3];
-    // console.log(firstButton.textContent, "test");
 
+    // increase question number count
     questionNum++;
 }
 
+// start the timer interval timer and the count down
 function startCountDown() {
-
     timerInterval = setInterval(function() {
-        timer.textContent = timer.textContent - 1;
-    
-        if(timer.textContent <= 0) {
-            // Stops execution of action at set interval
+        timer.textContent = timer.textContent - 1; // decrement the timer
+        if(timer.textContent <= 0) { // if timer reaches 0, load the finished screen
             loadFinished();
         }
-    
-    }, 1000);
-    return 25;
+    }, 1000); // set the timer interval to one second
+
+    return 25; // return the timer duration
 }
 
+// on click, start the quiz
 startButton.addEventListener("click", function() {
-    // console.log("test");
-    mainScreen.style.display = 'none';
-    quizScreen.style.display = 'flex';
-    timer.textContent = startCountDown();
-    loadQuestion();
+    mainScreen.style.display = 'none'; // hide the main screen
+    quizScreen.style.display = 'flex'; // show the quiz screen
+    timer.textContent = startCountDown(); // get the timer duration and start the count down
+    loadQuestion(); // load the first question
 })
 
+// when clicking on an answer option, display 
+// the result and load the next question
 options.addEventListener("click", function(event) {
-    clearInterval(resultInterval);
-    element = event.target;
-    console.log(element.textContent);
-    let result;
+    clearInterval(resultInterval); // clear the result popup of the previous click
+    element = event.target; // get the element the user clicked on
+    let result; // variable that will hold the result value
     if(answerKey[questions[questionNum-1]]===element.textContent) {
         result = "Correct!";
     } else {
         result = "Wrong!";
-        timer.textContent -= 5;
+        timer.textContent -= 5; // decrement the timer by 5, if wrong answer
     }
-    let clear = 0;
-    popupEl.style.cssText = "  \
+    let clear = 0; // variable to help clear the result popup
+    // set the css attributes of the result popup element 
+    // to show on screen
+    popupEl.style.cssText = "\
     display: flex;\
     flex-direction: column;\
     opacity: 0.4\
     ";
-    resultEl.textContent = result;
+    resultEl.textContent = result; // show the result on screen
     resultInterval = setInterval(function() {
-        
-        if(clear) {
-            // Stops execution of action at set interval
-            popupEl.style.display = 'none';
-            clearInterval(resultInterval);
-            console.log("clear");
+        if(clear) { // if clear flag is truthy, hide the result popup element
+            popupEl.style.display = 'none'; // hide the result popup elemnt
+            clearInterval(resultInterval); // clear the interval
         }
-        clear ++;
-    }, 900);
+        clear ++; // after one interval, set the clear flag to 1(truthy)
+    }, 900); // set the interval to 900 milliseconds
 
-    loadQuestion();
-
+    loadQuestion(); // load the next question
 })
 
+// submit the user's initials and score to local storage
 submitEl.addEventListener("click", function(event) {
-    event.preventDefault();
-    let scoreList;
+    event.preventDefault(); // prevent the page from refreshing after clicking
+    let scoreList; // variable to store the scores from local storage
     if (localStorage.getItem("score-list") === null) {
-        scoreList = [];
+        scoreList = []; // if local storage is empty, set the score list to empty
     } else {
+        // parse the scores from local storage to storage list
         scoreList = JSON.parse(localStorage.getItem("score-list"));
     }
-    const scoreString = `${document.querySelector("#input").value} - ${timer.textContent}`
+    // create a string to join the user's initials and the score
+    const scoreString = `${document.querySelector("#input").value} - ${timer.textContent}`;
+    // push that string to the scores list
     scoreList.push(scoreString);
+    // set the score list to local storage
     localStorage.setItem("score-list", JSON.stringify(scoreList));
-    window.location.href = "./highscores.html";
+    window.location.href = "./highscores.html"; // load the high scores page
 })
